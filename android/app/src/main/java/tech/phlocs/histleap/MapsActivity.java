66e7fd1cecd1cb0896650e94846f128f0f954268
zukizukki17 <k.matsumura0314@gmail.com>
2017-01-07 17:11:25 +0900

    created main view(header / footer).

--- android/app/src/main/java/tech/phlocs/histleap/MapsActivity.java
+++ android/app/src/main/java/tech/phlocs/histleap/MapsActivity.java
@@ -8,7 +8,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,8 +38,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng initialLocation = new LatLng(35.608834, 139.730238);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(initialLocation, (float)15));
     }
 }
