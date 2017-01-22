c5f935911b954303b7c3f57a2edeae3587101a1a
zukizukki17 <k.matsumura0314@gmail.com>
2017-01-22 14:15:18 +0900

    必要のないLog.dを撲滅 slider.javaのコンストラクタを修正。

--- android/app/src/main/java/tech/phlocs/histleap/model/Slider.java
+++ android/app/src/main/java/tech/phlocs/histleap/model/Slider.java
@@ -15,26 +15,11 @@ public class Slider {
     private ArrayList<Division> divisions;
 
     public Slider(ArrayList<Division> divisions) {
-//        ArrayList<Division> initialSlider = new ArrayList<>();
-//        initialSlider.add(new Division("飛鳥時代", 680, 710));
-//        initialSlider.add(new Division("奈良時代", 710, 794));
-//        initialSlider.add(new Division("平安時代", 794, 1192));
-//        initialSlider.add(new Division("鎌倉時代", 1192, 1336));
-//        initialSlider.add(new Division("室町時代", 1336, 1573));
-//        initialSlider.add(new Division("安土桃山時代", 1573, 1603));
-//        initialSlider.add(new Division("江戸時代", 1603, 1868));
-//        initialSlider.add(new Division("明治時代", 1868, 1912));
-//        initialSlider.add(new Division("大正時代", 1912, 1926));
-//        initialSlider.add(new Division("昭和時代", 1926, 1989));
-//        initialSlider.add(new Division("平成時代", 1989, 2017));
-        ArrayList<Division> initialSlider = divisions;
-
-        ArrayList<Integer> initialRange = new ArrayList<>();
-        initialRange.add(0);
-        initialRange.add(initialSlider.size() - 4);
-
-        this.divisions = initialSlider;
-        this.range = initialRange;
+        ArrayList<Integer> iRange = new ArrayList<>();
+        iRange.add(0);
+        iRange.add(divisions.size());
+        this.divisions = divisions;
+        this.range = iRange;
         this.isSamePoint = this.range.get(0).equals(this.range.get(1));
     }
 
